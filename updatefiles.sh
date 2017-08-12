20d4e92f1fa30ae86400bb25c99a8f8e8cb4695d
Franziska Schloesser <schloesser@zib.de>
2017-08-12 17:54:28 +0200

    update .vim structure and add undo to vim

--- updatefiles.sh
+++ updatefiles.sh
@@ -2,10 +2,11 @@
 
 # this script copies all listed dotfiles to the current directory
 
-FILES=".bashrc .bash_logout .bash_profile .git_completion.sh .gitconfig .git_prompt.sh .inputrc .profile .vimrc .Xmodmap"
+FILES=".bashrc .bash_logout .bash_profile .git_completion.sh .gitconfig .git_prompt.sh .inputrc .profile .Xmodmap"
 
 for FILE in ${FILES}
 do
     cp ${HOME}/${FILE} .
 done
 
+cp ${HOME}/.vim/vimrc .vim/
