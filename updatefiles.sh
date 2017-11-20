81c82587a3d1dbe681ac622d31e27448d9df0368
Franziska Schloesser <schloesser@zib.de>
2017-11-20 12:08:32 +0100

    remove colorscheme from updatefiles.sh

--- updatefiles.sh
+++ updatefiles.sh
@@ -2,7 +2,7 @@
 
 # this script copies all listed dotfiles to the current directory
 
-FILES=".bashrc .bash_logout .bash_profile .git_completion.sh .gitconfig .git_prompt.sh .inputrc .profile .Xmodmap .xinitrc .vim/vimrc .vim/colors/colibri.vim .config/i3/config .config/terminator/config"
+FILES=".bashrc .bash_logout .bash_profile .git_completion.sh .gitconfig .git_prompt.sh .inputrc .profile .Xmodmap .xinitrc .vim/vimrc .config/i3/config .config/terminator/config"
 
 for FILE in ${FILES}
 do
