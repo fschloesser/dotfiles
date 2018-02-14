60b9045be7ddfe9a76157bc1400a554a9d12f5a6
Franziska Schloesser <schloesser@zib.de>
2018-02-14 18:51:56 +0100

    make it work for more than one monitor

--- .regular/adjustbrightness.sh
+++ .regular/adjustbrightness.sh
@@ -5,13 +5,9 @@ HOUR=$(date +%H)
 
 # w should display the displays, weirdly enough this does not work
 # export DISPLAY=$(w "$(id -un)" | awk 'NF > 7 && $2 ~ /tty[0-9]+/ {print $3; exit}' 2>/dev/null)
-
 export DISPLAY=":0"
-setbrightness(){
-    # Adjust the brightness level for each monitor
-    OUT=$(xrandr --listactivemonitors | awk 'NR!=1{print " "$NF" "}')
-    for i in "${OUT[@]}"; do xrandr --output "${i// /}" --brightness "$1"; done
-}
+
+source ~/.bash_commands
 
 if [ 7 -le "${HOUR}" -a "${HOUR}" -lt 9 ]; then
     setbrightness 0.5
