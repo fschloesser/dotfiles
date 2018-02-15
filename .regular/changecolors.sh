eff5818c103743df05a681877ffe2a6c12c74c48
Franziska Schloesser <schloesser@zib.de>
2018-02-16 00:27:21 +0100

    add more configs for terminator

--- .regular/changecolors.sh
+++ .regular/changecolors.sh
@@ -5,6 +5,7 @@
 HOUR=$(date +%H)
 
 changecolor() {
+    # change vim color
     VIMS=$(vim --serverlist)
     for i in $VIMS; do
         vim --remote-send "<ESC>:set background=$1<CR>" --servername $i
@@ -13,6 +14,8 @@ changecolor() {
 
 if [ 8 -le "${HOUR}" -a "${HOUR}" -lt 17 ]; then
     changecolor light
+    cp ~/.config/terminator/config_day ~/.config/terminator/config
 else
     changecolor dark
+    cp ~/.config/terminator/config_night ~/.config/terminator/config
 fi
