b1b7430ba41698575b8dd34491c0e4164fa882c6
Franziska Schloesser <schloesser@zib.de>
2018-02-16 18:27:55 +0100

    add colors

--- .personal/adjustbrightness.sh
+++ .personal/adjustbrightness.sh
@@ -2,7 +2,7 @@
 
 time=$(date +%H%M)
 DAY=$(date +%m-%d)
-LINE=($(grep ^${DAY} ~/.regular/daylight_time.txt))
+LINE=($(grep ^${DAY} ~/.personal/daylight_time.txt))
 LIGHT=(xx 0.2 0.47 0.73 1 1 0.73 0.47 0.2)
 
 # w should display the displays, weirdly enough this does not work
@@ -27,26 +27,46 @@ function interpolate() {
 }
 
 source ~/.bash_commands
+source ~/.personal/colors.sh
 
-if (( ${time} < ${LINE[1]} )); then
-    intensity=0.2
-elif (( ${time} < ${LINE[2]} )); then
+color_fg=${rgb_lightpurple}
+color_bg=${rgb_darkpurple}
+
+# if (( ${time} < ${LINE[1]} )); then
+# if (( ${LINE[8]} < ${time} )); then
+intensity=0.2
+color_fg=${rgb_lightblue}
+color_bg=${rgb_darkblue}
+
+if (( ${time} < ${LINE[2]} )); then
     intensity=$(interpolate 1)
+    color_fg=${rgb_lightred}
+    color_bg=${rgb_darkorange}
 elif (( ${time} < ${LINE[3]} )); then
     intensity=$(interpolate 2)
+    color_fg=${rgb_lightyellow}
+    color_bg=${rgb_darkorange}
 elif (( ${time} < ${LINE[4]} )); then
     intensity=$(interpolate 3)
+    color_fg=${rgb_lightyellow}
+    color_bg=${rgb_darkblue}
 elif (( ${time} < ${LINE[5]} )); then
     intensity=1
+    color_fg=${rgb_lightpurple}
+    color_bg=${rgb_darkpurple}
 elif (( ${time} < ${LINE[6]} )); then
     intensity=$(interpolate 5)
+    color_fg=${rgb_lightyellow}
+    color_bg=${rgb_darkblue}
 elif (( ${time} < ${LINE[7]} )); then
     intensity=$(interpolate 6)
+    color_fg=${rgb_lightyellow}
+    color_bg=${rgb_darkorange}
 elif (( ${time} < ${LINE[8]} )); then
     intensity=$(interpolate 7)
-elif (( ${LINE[8]} < ${time} )); then
-    intensity=0.2
+    color_fg=${rgb_lightred}
+    color_bg=${rgb_darkorange}
 fi
-setbrightness ${intensity}
 
-xsetroot -bitmap ~/.config/background.bmp -fg "#aa99aa" -bg "#996699"
+setbrightness ${intensity}
+xsetroot -bitmap ~/.personal/background.bmp -fg "${color_fg}" -bg "${color_bg}"
---
+++ .personal/colors.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# this scripts defines and exports colors
+
+# green
+export rgb_lime="#ccff33"
+export rgb_forest="#446600"
+export rgb_bluegreen="#00cc99"
+
+# blues
+export rgb_turquoise="#009999"
+export rgb_darkblue="#222266"
+export rgb_lightblue="#bbbbee"
+export rgb_lightsky="#88ccff"
+
+# red-orange-yellow
+export rgb_red="#ff5500" # chimney
+export rgb_lightred="#ff9966" # lachs salmon
+export rgb_lightorange="#ffdd33"
+export rgb_lightyellow="#ffff66"
+
+
+# black-gray-white
+export rgb_black="#000000"
+export rgb_gray="#888888"
+export rgb_darkgray="#444444"
+export rgb_lightgray="#cccccc"
+export rgb_white="#ffffff"
+
+# purples
+export rgb_lightpurple="#aa99aa"
+export rgb_darkpurple="#996699"
+
