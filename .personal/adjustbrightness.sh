857b8de820dc90148dfb812eeeb737baa39960a9
Franziska Schloesser <schloesser@zib.de>
2018-03-02 08:58:33 +0100

    adjust brightness script

--- .personal/adjustbrightness.sh
+++ .personal/adjustbrightness.sh
@@ -3,7 +3,7 @@
 time=$(date +%H%M)
 DAY=$(date +%m-%d)
 LINE=($(grep ^${DAY} ~/.personal/daylight_time.txt))
-LIGHT=(xx 0.2 0.47 0.73 1 1 0.73 0.47 0.2)
+LIGHT=(xx 0.2 0.4 0.6 1.0 1.0 0.6 0.4 0.2)
 
 # w should display the displays, weirdly enough this does not work
 # export DISPLAY=$(w "$(id -un)" | awk 'NF > 7 && $2 ~ /tty[0-9]+/ {print $3; exit}' 2>/dev/null)
@@ -33,39 +33,48 @@ color_fg=${rgb_lightpurple}
 color_bg=${rgb_darkpurple}
 
 if (( 10#${time} < 10#${LINE[1]} || 10#${LINE[8]} < 10#${time} )); then
+    echo "one"
     intensity=0.2
     color_fg=${rgb_darkmidnight}
     color_bg=${rgb_lightmidnight}
 elif (( 10#${time} < 10#${LINE[2]} )); then
+    echo "two"
     intensity=$(interpolate 1)
     color_fg=${rgb_pink}
     color_bg=${rgb_rose}
 elif (( 10#${time} < 10#${LINE[3]} )); then
+    echo "three"
     intensity=$(interpolate 2)
     color_fg=${rgb_papaya_whip}
     color_bg=${rgb_moccasin}
 elif (( 10#${time} < 10#${LINE[4]} )); then
+    echo "vier"
     intensity=$(interpolate 3)
     color_fg=${rgb_alice_blue}
     color_bg=${rgb_lavender}
 elif (( 10#${time} < 10#${LINE[5]} )); then
+    echo "fuenf"
     intensity=1
     color_fg=${rgb_lightpurple}
     color_bg=${rgb_darkpurple}
 elif (( 10#${time} < 10#${LINE[6]} )); then
+    echo "sechs"
     intensity=$(interpolate 5)
     color_fg=${rgb_alice_blue}
     color_bg=${rgb_lavender}
 elif (( 10#${time} < 10#${LINE[7]} )); then
+    echo "sieben"
     intensity=$(interpolate 6)
     color_fg=${rgb_papaya_whip}
     color_bg=${rgb_moccasin}
 elif (( 10#${time} < 10#${LINE[8]} )); then
+    echo "acht"
     intensity=$(interpolate 7)
     color_fg=${rgb_pink}
     color_bg=${rgb_rose}
 fi
 
+echo $intensity
 setbrightness ${intensity}
 
 changecolor() {
