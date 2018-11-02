6892d05fd04a1692f47e1167a045f09d2a2c29b1
Franziska Schloesser <schloesser@zib.de>
2018-11-02 17:28:44 +0100

    update prompt

--- helpers/colors.sh
+++ helpers/colors.sh
@@ -5,20 +5,32 @@ do
     do
         echo -en "\e[0;${D};${E}m[0;${D};${E}]";
     done
-    echo ""
+    echo -e "\e[m"
 done
 for C in '48;5' '38;5';
 do
-    echo -e ""
+    echo -e "\e[m"
     for D in {1..25};
     do
-        echo -e ""
-        for E in 6 7 8 9 0 1 2 3 4 5;
+        echo -e "\e[m"
+        for E in 0 1 2 3 4 5 6 7 8 9;
         do
-            echo -en "\e[0;30;47m"
+            echo -en "\e[0;30;47m" # light background
             echo -en "\e[${C};${D}${E}m[${C};${D}${E}]";
-            echo -en "\e[0;37;40m"
+            echo -en "\e[1m\e[${C};${D}${E}m[${C};${D}${E}]";
+        done
+    done
+    echo -e "\e[m"
+    for D in {1..25};
+    do
+        echo -e "\e[m"
+        for E in 0 1 2 3 4 5 6 7 8 9;
+        do
+            echo -en "\e[0;37;40m" # dark background
             echo -en "\e[${C};${D}${E}m[${C};${D}${E}]";
+            echo -en "\e[1m\e[${C};${D}${E}m[${C};${D}${E}]";
         done
     done
 done
+
+echo -e "\e[m"
